5386b1dcec86c36cb6ed9cedccdcdad541854455
Guilherme Pereira Santanna <31224151+GuilhermeSantannape@users.noreply.github.com>
2019-10-08 14:09:19 -0300

    Add files via upload

--- pet.sql
+++ pet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Set-2019 às 03:01
+-- Tempo de geração: 07-Out-2019 às 00:10
 -- Versão do servidor: 10.4.6-MariaDB
 -- versão do PHP: 7.1.32
 
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `animais` (
   `id_animal` int(11) NOT NULL,
   `desc_animal` varchar(255) DEFAULT NULL,
-  `id_raca` varchar(50) NOT NULL
+  `id_raca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -44,17 +44,8 @@ CREATE TABLE `clientes` (
   `id` int(10) UNSIGNED NOT NULL,
   `nome` varchar(50) NOT NULL,
   `status` varchar(1) NOT NULL,
-  `id_animal` varchar(15) NOT NULL
+  `id_animal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `clientes`
---
-
-INSERT INTO `clientes` (`id`, `nome`, `status`, `id_animal`) VALUES
-(2, 'anotonio', 'A', '3'),
-(4, 'ele', 'A', '3'),
-(5, 'ROBERTO', 'A', '3');
 
 -- --------------------------------------------------------
 
@@ -67,24 +58,50 @@ CREATE TABLE `raca` (
   `desc_raca` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Extraindo dados da tabela `raca`
+-- Estrutura da tabela `tokens`
 --
 
-INSERT INTO `raca` (`id_raca`, `desc_raca`) VALUES
-(3, 'salchicha'),
-(5, 'salchicha'),
-(6, 'salchicha');
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL,
+  `token` varchar(300) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `data_expiracao` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `senha` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
+-- Índices para tabela `animais`
+--
+ALTER TABLE `animais`
+  ADD PRIMARY KEY (`id_animal`),
+  ADD KEY `id_raca` (`id_raca`);
+
+--
 -- Índices para tabela `clientes`
 --
 ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_animal` (`id_animal`);
 
 --
 -- Índices para tabela `raca`
@@ -93,20 +110,74 @@ ALTER TABLE `raca`
   ADD PRIMARY KEY (`id_raca`);
 
 --
+-- Índices para tabela `tokens`
+--
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `animais`
+--
+ALTER TABLE `animais`
+  MODIFY `id_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `raca`
 --
 ALTER TABLE `raca`
-  MODIFY `id_raca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_raca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `tokens`
+--
+ALTER TABLE `tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `animais`
+--
+ALTER TABLE `animais`
+  ADD CONSTRAINT `id_raca` FOREIGN KEY (`id_raca`) REFERENCES `raca` (`id_raca`);
+
+--
+-- Limitadores para a tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `id_animal` FOREIGN KEY (`id_animal`) REFERENCES `animais` (`id_animal`);
+
+--
+-- Limitadores para a tabela `tokens`
+--
+ALTER TABLE `tokens`
+  ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
