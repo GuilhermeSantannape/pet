b0d3c503b81ba36b5dadc1c4affbe2a63a129a78
guilherme santanna <guilhermesantanna.mail@gmail.com>
2019-10-17 11:30:48 -0300

    banco de dados

--- pet.sql
+++ pet.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16-Out-2019 às 07:47
+-- Tempo de geração: 17-Out-2019 às 16:29
 -- Versão do servidor: 10.4.6-MariaDB
--- versão do PHP: 7.3.9
+-- versão do PHP: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,6 +34,13 @@ CREATE TABLE `animais` (
   `id_raca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `animais`
+--
+
+INSERT INTO `animais` (`id_animal`, `nome_animal`, `id_raca`) VALUES
+(1, 'bob', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -51,17 +58,29 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id`, `nome`, `status`) VALUES
-(1, 'santanna', 'I');
+(1, 'santanna', 'I'),
+(10, 'Valmir', 'A'),
+(11, 'Valmir', 'A'),
+(12, 'kleber', 'A');
 
 -- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `cliente_animal`
 --
+-- Erro ao ler a estrutura para a tabela pet.cliente_animal: #1932 - Table 'pet.cliente_animal' doesn't exist in engine
+-- Erro ao ler dados para tabela pet.cliente_animal: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `pet`.`cliente_animal`' na linha 1
 
-CREATE TABLE `cliente_animal` (
-  `id` int(11) DEFAULT NULL,
-  `id_animal` int(11) DEFAULT NULL
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `ean` int(13) NOT NULL,
+  `nome` varchar(30) DEFAULT NULL,
+  `preco` int(13) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -134,6 +153,12 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`ean`);
+
+--
 -- Índices para tabela `raca`
 --
 ALTER TABLE `raca`
@@ -161,13 +186,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `animais`
 --
 ALTER TABLE `animais`
-  MODIFY `id_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `raca`
@@ -196,12 +221,6 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `animais`
   ADD CONSTRAINT `id_raca` FOREIGN KEY (`id_raca`) REFERENCES `raca` (`id_raca`);
-
---
--- Limitadores para a tabela `cliente_animal`
---
-ALTER TABLE `cliente_animal`
-  ADD CONSTRAINT `fk_animal` FOREIGN KEY (`id_animal`) REFERENCES `animais` (`id_animal`);
 
 --
 -- Limitadores para a tabela `tokens`
